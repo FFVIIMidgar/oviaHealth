12cc93b8a6cc35b43e2b0bf256259a4c81e864ad
Jonathan Smalls <jon@smalls.rocks>
2018-11-15 16:36:53 -0500

    feature: writing room cleaning logic https://github.com/anEffingChamp/oviaHealth
    All rooms must be cleaned after being occupied and prior to being rented again.
    The gnomes cleaning squad needs ONE hour per room plus THIRTY minutes per person
    in the room to clean it.
    There is only ONE gnome cleaning squad, but we might hire more.
    Legally the gnomes are only allowed to work EIGHT hours per day, but we’re
    always lobbying the local lord to let the gnomes work more. ( We’re really sure
    they want to )
    The gnomes start time is flexible but must be contiguous.
    
    reference:
    https://docs.google.com/document/d/1f6Atif7Y08C4AjAbTlqk-_3O1SYy55swKj4BN2Vk55I/edit?ts=5bedd786

--- models/inn.php
+++ models/inn.php
@@ -14,14 +14,53 @@ class inn
 /**
  * private property $_rooms contains the state of each of the four rooms of the
  * inn. They are numerically indexed from zero within this array.
+ *
+ * The inn has FOUR rooms
+ *
+ * ONE room sleeps TWO people and has ONE storage space
+ * ONE room sleeps TWO people and has ZERO storage space
+ * ONE room has TWO storage spaces and sleeps ONE person
+ * ONE room sleeps ONE person and has ZERO storage space
  */
 private $_rooms = array();
+/*
+ * $this->_cleaningTeam keeps track of how many hours the cleaning gnomes have
+ * worked today.
+ *
+ * There is only ONE gnome cleaning squad, but we might hire more.
+ * Legally the gnomes are only allowed to work EIGHT hours per day, but we’re
+ * always lobbying the local lord to let the gnomes work more. ( We’re really
+ * sure they want to )
+ * The gnomes start time is flexible but must be contiguous.
+ */
+private $_cleaningTeam = array(
+    'timeStart' => 0,
+    'room'      => 0,
+    'timeClean' => 0
+);
 function __construct()
 {
     include_once room.php;
+    /*
+     * We initialize each of the four rooms in the inn.
+     */
     $this->_rooms[] = new room(2, 1);
     $this->_rooms[] = new room(2, 0);
     $this->_rooms[] = new room(1, 2);
     $this->_rooms[] = new room(1, 0);
+    /*
+     * Then we can schedule an appropriate start time for the cleaning team.
+     */
+    $this->_cleaningTeam['timeStart'] = time();
+    for ($loop = 0;
+    $loop < count($this->_rooms);
+    $loop++
+    ) {
+        /*
+         * We check the clean status of each room, or compare the timestamp to
+         * verify that they are in fact clean.
+         */
+        $this->_rooms[$loop]->clean();
+    }
 }
 }
--- models/room.php
+++ models/room.php
@@ -12,8 +12,13 @@
  */
 class room
 {
-protected $_occupancy = 2;
-protected $_storage = 0;
+private $_occupancy = 2;
+private $_storage   = 0;
+/*
+ * $this->_clean marks the time stamp when a room was last cleaned. If that time
+ * has elapsed then we can rent the room again.
+ */
+private $_clean     = 0;
 function __construct($occupancy = 2, $storage = 0)
 {
     switch (true) {
@@ -38,8 +43,27 @@ function __construct($occupancy = 2, $storage = 0)
          */
         return false;
     default:
-        $this->_occupancy = $occupancy;
-        $this->_storage   = $storage;
     }
+    $this->_occupancy = $occupancy;
+    $this->_storage   = $storage;
+    $this->clean();
+}
+/**
+ * clean() estimates the time required after occupancy to clean a room for
+ * reuse. Once that time has elapsed we mark $this->_clean = true.
+ *
+ * All rooms must be cleaned after being occupied and prior to being rented
+ * again.
+ * The gnomes cleaning squad needs ONE hour per room plus THIRTY minutes per
+ * person in the room to clean it.
+ */
+function clean()
+{
+    // TODO Presumably we have some form of persistent storage, so we can keep
+    // track of whether a room is clean between program runs. For this example I
+    // will assume that we have a query() function that already has a database
+    // connection, and will return what I need.
+    $this->_clean = time() + (3600 + (1800 * $this->_occupancy));
+    return $this->_clean;
 }
 }
